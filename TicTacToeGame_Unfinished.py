3b53e81ad427d17b5dfd52a2e80b9478781e5593
adityapathak343 <adityapathak343@users.github.com>
2018-07-15 13:30:00 +0530

    Practice Repository for Inter DPS Competition

---
+++ TicTacToeGame_Unfinished.py
@@ -0,0 +1,43 @@
+#defining grid and other funcrtions
+from os import system, name
+a = [1,2,3,4,5,6,7,8,9]
+def prgrd():
+    print("     |     |     ")
+    print("_",a[6],"_|_",a[7],"_|_",a[8],"_")
+    print("     |     |     ")
+    print("_",a[3],"_|_",a[4],"_|_",a[5],"_")  
+    print("     |     |     ")
+    print(" ",a[0]," | ",a[1], " | ",a[2]," ")
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+ 
+    else:
+        _ = system('clear')
+def wfx():
+    if ((a[0] == 'X' and a[1] == 'X' and a[2] == 'X')or(a[3] == 'X' and a[4] == 'X' and a[5] == 'X')or(a[6] == 'X' and a[7] == 'X' and a[8] == 'X')or(a[0] == 'X' and a[3] == 'X' and a[6] == 'X')or(a[1] == 'X' and a[4] == 'X' and a[7] == 'X')or(a[2] == 'X' and a[5] == 'X' and a[8] == 'X')or(a[0] == 'X' and a[4] == 'X' and a[8] == 'X')or(a[2] == 'X' and a[4] == 'X' and a[6] == 'X')):
+        print('X has won!')
+        raise SystemExit
+def wfy():
+    if ((a[0] == 'O' and a[1] == 'O' and a[2] == 'O')or(a[3] == 'O' and a[4] == 'O' and a[5] == 'O')or(a[6] == 'O' and a[7] == 'O' and a[8] == 'O')or(a[0] == 'O' and a[3] == 'O' and a[6] == 'O')or(a[1] == 'O' and a[4] == 'O' and a[7] == 'O')or(a[2] == 'O' and a[5] == 'O' and a[8] == 'O')or(a[0] == 'O' and a[4] == 'O' and a[8] == 'O')or(a[2] == 'O' and a[4] == 'O' and a[6] == 'O')): 
+        print('O has won')
+        raise SystemExit
+prgrd()
+ctr = 1
+
+#maincode:
+while(ctr < 9):
+    x = int(input("Where would you like to place X? "))
+    if x in a:
+        a[x-1] = 'X'
+    clear() 
+    prgrd()
+    wfx()
+    o = int(input("Where would you like to place O? "))
+    if o in a:
+        a[o-1] = 'O'
+    clear() 
+    prgrd()
+    wfy()
+    ctr = ctr + 1
+print("The match was tied!")
