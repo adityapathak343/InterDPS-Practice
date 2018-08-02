6a5839472e1325727e750c47b3b060e37064cab6
adityapathak343 <39326010+adityapathak343@users.noreply.github.com>
2018-08-02 12:55:14 +0530

    Add files via upload
    iuhgqa

--- IntToStr.py
+++ IntToStr.py
@@ -1,30 +1,27 @@
 #simplification: bottom to top
-def inv(s):
-    st=""
-    for letter in s:
-        st=letter+st
-    return st
 def itw(x):
     s=''
     l=len(x)-1
     ones=['','one', 'two', 'three', 'four', 'five', 'six','seven','eight','nine']
     tens=['', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
     ltens=['ten', 'eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
-    suff=['',tens[int(x[len(x)-2])], 'hundred and','thousand', '']
+    suff=['',tens[int(x[-2])], 'hundred and','thousand', '']
     for letter in x:
             s=ones[int(letter)]+" "
             if l == 1:
                 s=''
-                if int(x[len(x)-2])==1:
+                if int(x[-2])==1:
                     s=''
-                    print(ltens[int(x[len(x)-1])])
-                    raise SystemExit
+                    print(ltens[int(x[-1])])
+                    exit
             t = suff[l]
-            if s==' ':
+            if s==' ' and l==2:
                 t='and'
+                if int(x[-1]) == 0:
+                    t=''
             print (s+t, end=" ")
             l=l-1     
             if l<0:
-                raise SystemExit       
+                exit       
 y  = input("Enter a number..")
 itw(y)
